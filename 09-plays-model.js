1f7618a0c4e2eef32136be724adcbac908b822e6
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-02-02 14:51:55 +0100

    typo

--- 09-plays-model.js
+++ 09-plays-model.js
@@ -2,6 +2,6 @@ const plays = [];
 const play_schema = {arg: 'string', expected:'string', actual: 'string', correct: 'boolean'};
 
 console.groupCollapsed('%c plays: array of game play records', 'font-weight:bold');
-console.log('play_schema: ', plays_schema);
+console.log('play_schema: ', play_schema);
 console.groupEnd();
 run_tests(str_identity, str_identity_tests, false);
