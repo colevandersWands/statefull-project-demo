dac8748a7a01c96cf78f64de0fa57e421949dbdb
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-02-02 14:37:48 +0100

    schemas

--- 09-plays-model.js
+++ 09-plays-model.js
@@ -1,7 +1,7 @@
 const plays = [];
-const plays_schema = {arg: 'string', expected:'string', actual: 'string', correct: 'boolean'};
+const play_schema = {arg: 'string', expected:'string', actual: 'string', correct: 'boolean'};
 
 console.groupCollapsed('%c plays: array of game play records', 'font-weight:bold');
-console.log('schema: ', plays_schema);
+console.log('play_schema: ', plays_schema);
 console.groupEnd();
 run_tests(str_identity, str_identity_tests, false);
